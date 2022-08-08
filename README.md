4734c014e966390b33f729a53c1ca6d38fb095f9
Jackson G. Kaindume <seestem@merely.tech>
2022-08-08 12:02:54 +0200

    Convert .md to html, instead of using .txt files

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ welcome to __seestem__'s website
 
 __[projects]__
 
-- [htoml](./projects/htoml.txt): HTML page templates described with a toml config. [[git](https://codeberg.org/seestem/htoml)]
-- [crawleth](./projects/crawleth.txt): Ethereum crawler. [[git](https://codeberg.org/seestem/crawleth)]
+- [htoml](./projects/htoml.html): HTML page templates described with a toml config. [[git](https://codeberg.org/seestem/htoml)]
+- [crawleth](./projects/crawleth.html): Ethereum crawler. [[git](https://codeberg.org/seestem/crawleth)]
 
 ___
 
 email:  seestem (at) merely (dot) tech
 
 ---
-
-last update: 2022-08-06
+Last updated: 2022-08-08
--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Remove last line of README (last update date)
+sed -i '$ d' README.md
+
+# Append date to README
+echo "Last updated: $(date -I)" >> README.md
+
 # Build home page
 pandoc -s --metadata title="seestem" -i README.md -o index.html  --self-contained --css=home.css
 
@@ -7,7 +13,7 @@ pandoc -s --metadata title="seestem" -i README.md -o index.html  --self-containe
 pandoc -s --metadata title="Oryx App Privacy Policy" -i oryx-privacy.md  -o projects/oryx/index.html  --self-contained --css=home.css
 
 # build htoml
-cp ../htoml/README ./projects/htoml.txt
+pandoc -s --metadata title="htoml" -i ../htoml/README.md -o projects/htoml.html  --self-contained --css=home.css
 
 # build crawleth
-cp ../crawleth/README ./projects/crawleth.txt
+pandoc -s --metadata title="crawleth" -i ../crawleth/README.md -o projects/crawleth.html  --self-contained --css=home.css
