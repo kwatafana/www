740aa795d4dd451e70ab539b74e1c67afb3bd7d8
Jackson G. Kaindume <seestem@merely.tech>
2022-08-14 21:41:39 +0200

    code theme

--- build.sh
+++ build.sh
@@ -13,16 +13,16 @@ pandoc -s --metadata title="seestem" -i README.md -o index.html  --self-containe
 pandoc -s --metadata title="Oryx App Privacy Policy" -i oryx-privacy.md  -o projects/oryx/index.html  --self-contained --css=home.css
 
 # build htoml
-pandoc -s --metadata title="htoml" -i ../htoml/README.md -o projects/htoml.html  --self-contained --css=home.css
+pandoc -s --metadata title="htoml" -i ../htoml/README.md -o projects/htoml.html  --self-contained --css=code.css
 
 # build crawleth
-pandoc -s --metadata title="crawleth" -i ../crawleth/README.md -o projects/crawleth.html  --self-contained --css=home.css
+pandoc -s --metadata title="crawleth" -i ../crawleth/README.md -o projects/crawleth.html  --self-contained --css=code.css
 
 # build h4
-pandoc -s --metadata title="h4" -i ../h4ck1ng/README.md -o projects/h4.html  --self-contained --css=home.css
+pandoc -s --metadata title="h4" -i ../h4ck1ng/README.md -o projects/h4.html  --self-contained --css=code.css
 
 # build cyrtophora
-pandoc -s --metadata title="cyrtophora" -i ../cyrtophora/README.md -o projects/cyrtophora.html  --self-contained --css=home.css
+pandoc -s --metadata title="cyrtophora" -i ../cyrtophora/README.md -o projects/cyrtophora.html  --self-contained --css=code.css
 
 
 ###################### Writings ######################
