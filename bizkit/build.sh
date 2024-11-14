94494aad6317db89a87ef3a7894bd865596b9718
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-11-14 23:39:41 +0200

    update

--- bizkit/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pandoc -i README.md -o ../www/index.html \
-       -s --css=blog.css \
-       --metadata title="🍪 Introducing Bizkit" \
-       -H header.html
-
-cp registration.html ../www/
